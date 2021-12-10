c778841cd3646cf9a315bac5453e9f533f6d5870
Tran Duc Manh <tranducmanh12092001@gmail.com>
2021-12-10 20:35:15 +0700

    Added Query files

---
+++ Query/Create Tables.sql
@@ -0,0 +1,21 @@
+create database CSDL_Project;
+
+use CSDL_Project;
+
+create schema production; 
+go
+
+create schema sales; 
+go
+
+create table sales.customers (
+	customer_id int identity(1,1),
+	name nvarchar(255) not null,
+	birthday date not null,
+	address nvarchar(255) not null,
+	phone varchar(25) not null,
+	email varchar(255) not null,
+	constraint PK0 primary key (customer_id),
+);
+
+drop table sales.customers;
