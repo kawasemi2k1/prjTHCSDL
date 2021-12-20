98cff78697126f647f61e8495eb609f841f9bb08
Tran Duc Manh <tranducmanh12092001@gmail.com>
2021-12-20 15:48:39 +0700

    Updated Query

--- Query/Load Datas.sql
+++ Query/Load Datas.sql
@@ -16,7 +16,7 @@ SET IDENTITY_INSERT sales.stores Off;
 SET IDENTITY_INSERT sales.customers ON;
 
 insert into sales.customers values
-(N'Vô Danh', null, null, null, null),
+(N'Khách lẻ', null, null, null, null);
 (N'Trần Đức Mạnh', '20011209', N'15 ngách 2 ngõ 119, đường Nước Phần Lan, Tấy Hồ, Hà Nội', '0357251858', 'tranducmanh12092001@gmail.com'),
 (N'Phạm Đức Huy', '20010101', N'Việt Nam', '0000000001', 'phamduchuy@gmail.com'),
 (N'Nguyễn Thị Thúy', '20010101', N'Việt Nam', '0000000002', 'nguyenthithuy@gmail.com'),
