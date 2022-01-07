ee4bf8ab8686be129182c24372e40ade31a7339d
Tran Duc Manh <tranducmanh12092001@gmail.com>
2022-01-07 09:16:39 +0700

    Updated Database

--- Query/Create Tables.sql
+++ Query/Create Tables.sql
@@ -1,12 +1,4 @@
-create database CSDL_Project;
-
 use CSDL_Project;
-
-CREATE SCHEMA production;
-go
-
-CREATE SCHEMA sales;
-go
 
 CREATE TABLE production.categories (
 	category_id INT IDENTITY (1, 1) PRIMARY KEY,
@@ -25,8 +17,8 @@ CREATE TABLE production.products (
 	brand_id INT NOT NULL,
 	category_id INT NOT NULL,
 	price DECIMAL (10, 2) NOT NULL,
-	FOREIGN KEY (category_id) REFERENCES production.categories (category_id) ON DELETE CASCADE ON UPDATE CASCADE,
-	FOREIGN KEY (brand_id) REFERENCES production.brands (brand_id) ON DELETE CASCADE ON UPDATE CASCADE
+	FOREIGN KEY (category_id) REFERENCES production.categories (category_id),
+	FOREIGN KEY (brand_id) REFERENCES production.brands (brand_id)  
 );
 
 create table sales.customers (
@@ -38,10 +30,6 @@ create table sales.customers (
 	email varchar(255) constraint UQ1 unique,
 	constraint PK0 primary key (customer_id),
 );
-go
-
-create view vRealCustomer as
-select * from sales.customers where customer_id != 1
 
 CREATE TABLE sales.stores (
 	store_id INT IDENTITY (1, 1) PRIMARY KEY,
@@ -62,7 +50,7 @@ CREATE TABLE sales.staffs (
 	manager_state INT Not null,
 	gender nvarchar (10) not null,
 	password varchar (20) not null,
-	FOREIGN KEY (store_id) REFERENCES sales.stores (store_id) ON DELETE CASCADE ON UPDATE CASCADE
+	FOREIGN KEY (store_id) REFERENCES sales.stores (store_id)  
 );
 
 CREATE TABLE sales.orders (
@@ -70,12 +58,11 @@ CREATE TABLE sales.orders (
 	customer_id INT not null,
 	staff_id INT NOT NULL,
 	created_date DATE NOT NULL,
-	price decimal(10,2) not null default 0,
-	FOREIGN KEY (customer_id) REFERENCES sales.customers (customer_id) ON DELETE CASCADE ON UPDATE CASCADE,
-	FOREIGN KEY (staff_id) REFERENCES sales.staffs (staff_id) ON DELETE NO ACTION ON UPDATE NO ACTION
+	FOREIGN KEY (customer_id) REFERENCES sales.customers (customer_id)  ,
+	FOREIGN KEY (staff_id) REFERENCES sales.staffs (staff_id) 
 );
 
-CREATE TABLE sales.stocks (
+CREATE TABLE sales.products (
 	product_id INT not null,
 	created_at date not null,
 	good_till date not null,
@@ -84,8 +71,8 @@ CREATE TABLE sales.stocks (
 	discount DECIMAL (4, 2) NOT NULL DEFAULT 0,
 	quantity INT not null default 0,
 	PRIMARY KEY (product_id, created_at, good_till, store_id),
-	FOREIGN KEY (store_id) REFERENCES sales.stores (store_id) ON DELETE CASCADE ON UPDATE CASCADE,
-	FOREIGN KEY (product_id) REFERENCES production.products (product_id) ON DELETE CASCADE ON UPDATE CASCADE
+	FOREIGN KEY (store_id) REFERENCES sales.stores (store_id)  ,
+	FOREIGN KEY (product_id) REFERENCES production.products (product_id)  
 );
 
 CREATE TABLE sales.order_items (
@@ -97,13 +84,14 @@ CREATE TABLE sales.order_items (
 	quantity INT NOT NULL default 0,
 	price DECIMAL (10, 2) NOT NULL default 0,
 	discount DECIMAL (4, 2) NOT NULL DEFAULT 0,
-	FOREIGN KEY (order_id) REFERENCES sales.orders (order_id) ON DELETE CASCADE ON UPDATE CASCADE,
-	foreign key (product_id, created_at, good_till, store_id) references sales.stocks (product_id, created_at, good_till, store_id),
+	profit DECIMAL (10, 2) NOT NULL default 0,
+	FOREIGN KEY (order_id) REFERENCES sales.orders (order_id) on delete cascade on update cascade,
+	foreign key (product_id, created_at, good_till, store_id) references sales.products (product_id, created_at, good_till, store_id),
 	foreign key (product_id) references production.products (product_id)
 );
 
 drop table sales.order_items;
-drop table sales.stocks;
+drop table sales.products;
 drop table sales.orders;
 drop table sales.staffs;
 drop table production.products;
@@ -111,3 +99,18 @@ drop table production.categories;
 drop table production.brands;
 drop table sales.customers;
 drop table sales.stores;
+
+--Views
+go
+create view vRealCustomer as
+select * from sales.customers where customer_id != 1;
+go
+create view vOutdatedProduct as
+select * from sales.products where getdate() >= good_till;
+go
+create view vCurrentProduct as
+select * from sales.products where getdate() < good_till;
+
+drop view vRealCustomer;
+drop view vOutdatedProduct;
+drop view vCurrentProduct;
